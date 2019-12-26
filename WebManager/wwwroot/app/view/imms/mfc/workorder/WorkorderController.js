2addf5cd0cf671cb94e5129813403233a9c51054
yahong2018 <SoftwareChina@qq.com>
2019-12-27 01:01:27 +0800

    一些完善

--- WebManager/wwwroot/app/view/imms/mfc/workorder/WorkorderController.js
+++ WebManager/wwwroot/app/view/imms/mfc/workorder/WorkorderController.js
@@ -1,7 +1,7 @@
 Ext.define('app.view.imms.mfc.workorder.WorkorderController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.imms_mfc_workorder_WorkorderController',
-    uses:["app.ux.excel.Importer"],
+    uses: ["app.ux.excel.Importer"],
     startOrder: function () {
         var url = "api/imms/mfc/workorder/start";
         var grid = this.getView();
@@ -23,13 +23,24 @@ Ext.define('app.view.imms.mfc.workorder.WorkorderController', {
                             width: 200,
                             align: 't'
                         });
+
+                        var button = grid.down("[buttonId='BTN_START']");
+                        button.setDisabled(true);
                     }
                 });
-            }            
+            }
         });
     },
     importOrder: function () {
-        var importer = Ext.create({ xtype:"app_ux_excel_impoter"});
+        var importer = Ext.create({ xtype: "app_ux_excel_impoter" });
         importer.show();
-    }
+    },
+
+    gridSelectionChanged: function (model, selected, index) {
+        var grid = this.getView();
+        var record = grid.getSelectedRecord();
+        var status = record.get("orderStatus");
+        var button = grid.down("[buttonId='BTN_START']");
+        button.setDisabled(status != 0);
+    },
 });
